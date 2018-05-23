91901d5c8e1c76066e26a2c1b051bc1fdf3ffe4a
Mehul Mohan <mehulmpt@gmail.com>
2018-05-24 03:43:43 +0530

    MongoDB integrated

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Waves 2018 website coming soon')
-})
+const mongoose = require('mongoose')
 
-app.listen(1337, () => console.log('Listening on [::1]:1337'))
+(async function() {
+    await mongoose.connect('mongodb://localhost/waves') // mongo up
+    
+    app.get('/', (req, res) => {
+        console.log('Waves 2018 website coming soon!')
+    })
+
+    app.listen(1337, () => console.log('Listening on [::1]:1337'))
+})()
